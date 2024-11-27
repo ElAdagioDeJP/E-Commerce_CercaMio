66babf73dd2bbcb7d94241a4ce4ba9f353ccdfac
FreddyJr27 <freddyjr200427@gmail.com>
2024-11-26 21:36:15 -0400

    Se arreglo el registro de usuario en react

--- frontend/src/Registro.jsx
+++ frontend/src/Registro.jsx
@@ -9,9 +9,14 @@ const RegistroUsuario = () => {
   // Estados para manejar los datos del formulario
   const [formData, setFormData] = useState({
     nombre: '',
+    apellido: '',
+    usuario: '',
     email: '',
+    direccion: '',
+    fechanacimiento: '',
     password: '',
-    confirmPassword: ''
+    confirmPassword: '',
+    telefono: ''
   });
 
   // Manejar cambios en los inputs
@@ -25,7 +30,9 @@ const RegistroUsuario = () => {
     e.preventDefault();
 
     // Validación básica
-    if (!formData.nombre || !formData.email || !formData.password || !formData.confirmPassword) {
+    if (!formData.nombre || !formData.email || !formData.password || !formData.confirmPassword 
+      || !formData.apellido || !formData.usuario || !formData.telefono || !formData.direccion 
+      || !formData.fechanacimiento) {
       alert("Por favor, completa todos los campos.");
       return;
     }
@@ -47,7 +54,7 @@ const RegistroUsuario = () => {
   };
 
   return (
-    <div>
+    <div className='cuerpo'>
 
       <Navbar /> {/* Aquí debe aparecer el Navbar */}
 
@@ -65,6 +72,16 @@ const RegistroUsuario = () => {
               onChange={handleInputChange}
             />
 
+            <label htmlFor="apellido" className='login-label'>Apellido</label>
+            <input 
+            type="text"
+            id='apellido'
+            className='login-input'
+            placeholder='Ingrese su apellido'
+            value={formData.apellido}
+            onChange={handleInputChange}
+             />
+
             <label htmlFor="email" className="login-label">Correo Electrónico</label>
             <input
               type="email"
@@ -73,6 +90,45 @@ const RegistroUsuario = () => {
               placeholder="Ingrese su correo"
               value={formData.email}
               onChange={handleInputChange}
+            />
+
+            <label htmlFor="usuario" className='login-label'>Nombre del usuario</label>
+            <input
+            type='text'
+            id='usuario'
+            className='login-input'
+            placeholder='Ingrese su usuario'
+            value={formData.usuario}
+            onChange={handleInputChange}
+            />
+
+            <label htmlFor="telefono" className='login-label'>Telefono</label>
+            <input
+            type='tel'
+            id='telefono'
+            className='login-input'
+            placeholder='Ingrese su numero de telefono'
+            value={formData.telefono}
+            onChange={handleInputChange}
+            />
+            <label htmlFor="direccion" className='login-label'>Direccion</label>
+            <input
+            type='text'
+            id='direccion'
+            className='login-input'
+            placeholder='Ingrese su direccion: estado, municipio, parroquia'
+            value={formData.direccion}
+            onChange={handleInputChange}
+            />
+
+            <label htmlFor="fechanacimiento" className='login-label'>Fecha de nacimiento</label>
+            <input
+            type='date'
+            id='fechanacimiento'
+            className='login-input'
+            placeholder='Ingrese su fecha de nacimiento'
+            value={formData.fechanacimiento}
+            onChange={handleInputChange}
             />
 
             <label htmlFor="password" className="login-label">Contraseña</label>
